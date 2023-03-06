166ec6c4c2657bddd832b571c17c199dad26296e
Kauã Werle <kaua@digitaldoc.com.br>
2023-03-06 15:50:52 -0300

    fix connection

---
+++ config/connection.ts
@@ -0,0 +1,6 @@
+import { Sequelize } from 'sequelize';
+
+let db = new Sequelize('postgres://trash_it_user:HYYo1VPOIQhBZqaFl9ZLC776oZPlYRc9@dpg-cg32s24eoogop18annk0-a/trash_it');
+
+
+	export { db };
