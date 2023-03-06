182c768702b632b727226ec3201205c0be009a97
Kauã Werle <kaua@digitaldoc.com.br>
2023-03-06 15:42:12 -0300

    fix connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-let db = new Sequelize('postgres://postgres:root@localhost:5432/trash-it');
+let db = new Sequelize('postgres://trash_it_user:HYYo1VPOIQhBZqaFl9ZLC776oZPlYRc9@dpg-cg32s24eoogop18annk0-a/trash_it');
 
 
 	export { db };
