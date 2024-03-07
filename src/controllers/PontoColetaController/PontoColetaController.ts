f5f161d2d47a91416cea1152f1ec8c8f8c739dcd
Kauã Werle <kauawerle19@gmail.com.br>
2024-03-06 21:04:03 -0300

    fix errors in add point

--- src/controllers/PontoColetaController/PontoColetaController.ts
+++ src/controllers/PontoColetaController/PontoColetaController.ts
@@ -1,22 +1,18 @@
 import { Request, Response } from "express";
 import { v4 } from "uuid";
-import { QueryTypes } from "sequelize";
 
-import { PontoColetaModel } from "../../database/model/tb_ponto_coleta";
-import { PontoCategoriesModel } from "../../database/model/tb_ponto_categoria";
 import { CategoriesModel } from "../../database/model/tb_categoria";
+import { PontoCategoriesModel } from "../../database/model/tb_ponto_categoria";
+import { PontoColetaModel } from "../../database/model/tb_ponto_coleta";
 
 class PontoColetaController {
-	async index(req: Request, res: Response) {
-
-	}
+	async index(req: Request, res: Response) {}
 
 	async findAll(req: Request, res: Response) {
 		try {
 			const pontos = await PontoColetaModel.findAll();
 			res.json(pontos);
-		}
-		catch (err) {
+		} catch (err) {
 			return res.status(500).json(err);
 		}
 	}
@@ -30,22 +26,28 @@ class PontoColetaController {
 				res.json(pontos);
 			} else {
 				const categoryFiltered = await PontoCategoriesModel.findAll({
-					where: [{ "id_category": id_category }],
-					include: [{
-						model: PontoColetaModel,
-					}]
-				})
+					where: [{ id_category: id_category }],
+					include: [
+						{
+							model: PontoColetaModel,
+						},
+					],
+				});
 
-				const uniqueCategories = Object.values(categoryFiltered).reduce((acc, curr: any) => {
-					const index = acc.findIndex(a => a.id_category !== curr.id_category);
-					if (index < 0 && curr.id_category) {
-						acc.push(curr)
+				const uniqueCategories = Object.values(categoryFiltered).reduce(
+					(acc, curr: any) => {
+						const index = acc.findIndex(
+							(a) => a.id_category !== curr.id_category
+						);
+						if (index < 0 && curr.id_category) {
+							acc.push(curr);
+						}
+						return acc;
+					},
+					[]
+				);
 
-					}
-					return acc
-				}, [])
-
-				res.status(200).json(uniqueCategories)
+				res.status(200).json(uniqueCategories);
 			}
 		} catch (err) {
 			return res.status(500).json(err);
@@ -57,14 +59,15 @@ class PontoColetaController {
 			const { id_ponto } = req.query;
 
 			const categoryFiltered = await CategoriesModel.findAll({
-				include: [{
-					where: [{ "id_ponto": id_ponto }],
-					model: PontoCategoriesModel,
-				}]
-			})
+				include: [
+					{
+						where: [{ id_ponto: id_ponto }],
+						model: PontoCategoriesModel,
+					},
+				],
+			});
 
-			res.status(200).json(categoryFiltered)
-
+			res.status(200).json(categoryFiltered);
 		} catch (err) {
 			return res.status(500).json(err);
 		}
@@ -73,8 +76,8 @@ class PontoColetaController {
 	async findById(req: Request, res: Response) {
 		try {
 			const { id } = req.params;
-			PontoColetaModel.findByPk(id).then(result => {
-				res.status(200).json(result)
+			PontoColetaModel.findByPk(id).then((result) => {
+				res.status(200).json(result);
 			});
 		} catch (err) {
 			return res.status(500).json(err);
@@ -95,9 +98,8 @@ class PontoColetaController {
 				status,
 				email,
 				cellphone,
-				description
+				description,
 			} = req.body;
-
 
 			const pontos = {
 				id: v4(),
@@ -112,46 +114,60 @@ class PontoColetaController {
 				email,
 				cellphone,
 				status,
-				description
+				description,
 			};
+
+			// let rawQuery = "SELECT * FROM tb_users WHERE id = :id_user";
+			// let userInfo;
+			// db.query(rawQuery, {
+			// 	type: sequelize.QueryTypes.SELECT,
+			// 	replacements: { id_user: id_user },
+			// })
+			// 	.then((results) => {
+			// 		userInfo = results;
+			// 		console.log("Query results:", userInfo);
+			// 	})
+			// 	.catch((error) => {
+			// 		console.error("Error executing raw query:", error);
+			// 	});
+			// let user;
+			// userInfo.map((res) => {
+			// 	user = res.adm;
+			// });
 
 			const pontosEmpty = Object.values(pontos).some((val) => {
 				return typeof val === "undefined" || val === "";
-			})
-
+			});
 			if (pontosEmpty) {
-				res.status(400).json({ message: "Informe todos os campos obrigatórios" })
+				res
+					.status(400)
+					.json({ message: "Informe todos os campos obrigatórios" });
 			} else {
-				const insertedIds: any = await PontoColetaModel.create(pontos)
-				const pontoId = insertedIds.id
+				const insertedIds: any = await PontoColetaModel.create(pontos);
+				const pontoId = insertedIds.id;
 
-				const pointItems = items
-					.split(',')
-					.map((item_id: string) => {
-						return {
-							id_category: item_id,
-							id_ponto: pontoId,
-						}
-					})
+				let pointItems = items.split(",").map((item_id: string) => {
+					return {
+						id_category: item_id,
+						id_ponto: pontoId,
+					};
+				});
 
 				Object.values(pointItems).map((point_category: any) => {
-					const id_category = point_category.id_category
-					const id_ponto = point_category.id_ponto
+					const id_category = point_category.id_category;
+					const id_ponto = point_category.id_ponto;
 					PontoCategoriesModel.create({
 						id_category,
-						id_ponto
-					})
-
-				})
-
+						id_ponto,
+					});
+				});
 
 				return res.json({
 					id: pontoId,
-					...pontos
-				})
+					...pontos,
+				});
 			}
-		}
-		catch (err) {
+		} catch (err) {
 			return res.status(500).json({ err: err.message });
 		}
 	}
@@ -160,40 +176,37 @@ class PontoColetaController {
 		try {
 			const { id } = req.query;
 
-			const destroyCategories =
-				await PontoCategoriesModel.destroy({
-					where: { id_ponto: id }
-				})
+			const destroyCategories = await PontoCategoriesModel.destroy({
+				where: { id_ponto: id },
+			});
 
-			const destroyPoints =
-				await PontoColetaModel.destroy({
-					where: { id: id }
-				})
+			const destroyPoints = await PontoColetaModel.destroy({
+				where: { id: id },
+			});
 
-			res.status(200).json([destroyPoints, destroyCategories])
+			res.status(200).json([destroyPoints, destroyCategories]);
 		} catch (err) {
 			return res.status(500).json(err);
 		}
 	}
 	async updateStatus(req: Request, res: Response) {
 		try {
-			const {
-				status
-			} = req.body;
+			const { status } = req.body;
 
-			await PontoColetaModel.update({
-				status
-			}, {
-				where: {
-					id: req.params.id
+			await PontoColetaModel.update(
+				{
+					status,
+				},
+				{
+					where: {
+						id: req.params.id,
+					},
 				}
-			}).then(result => {
-				res.status(200).json(result)
+			).then((result) => {
+				res.status(200).json(result);
 			});
-
 		} catch (err) {
 			return res.status(500).json(err);
-
 		}
 	}
 }
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,16 +1,25 @@
 import { Sequelize } from 'sequelize';
 
 const db = new Sequelize({
-  database: "trash_it",
-  username: "trash_it_user",
-  password: "HYYo1VPOIQhBZqaFl9ZLC776oZPlYRc9",
-  host: "dpg-cg32s24eoogop18annk0-a.ohio-postgres.render.com",
-  port: 5432,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: true,
-  },
+	database: "trash-it",
+	username: "postgres",
+	password: "root",
+	host: "localhost",
+	port: 5432,
+	dialect: "postgres",
 });
+
+// const db = new Sequelize({
+//   database: "trash_it",
+//   username: "trash_it_user",
+//   password: "HYYo1VPOIQhBZqaFl9ZLC776oZPlYRc9",
+//   host: "dpg-cg32s24eoogop18annk0-a.ohio-postgres.render.com",
+//   port: 5432,
+// 	dialect: "postgres",
+//   dialectOptions: {
+//     ssl: true,
+//   },
+// });
 
 
 export { db };
